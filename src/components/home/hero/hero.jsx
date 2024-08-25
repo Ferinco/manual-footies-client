60762366292a04cdcacb5dc92b009dc637432696
Ferinco <emmanuelferrum003@gmail.com>
2024-08-25 01:17:07 +0100

    restyling homepage

--- src/components/home/hero/hero.jsx
+++ src/components/home/hero/hero.jsx
@@ -1,13 +1,14 @@
-import Background from "./background";
+import LeftSide from "./left";
 import MobileBanner from "./mobile-banner";
 import HeroOverlay from "./overlay";
+import RightSide from "./right";
 
 export default function HeroSection(){
     return(
-        <div className="container flex flex-col relative w-full mt-10 h-auto">
-            <Background/>
-            <HeroOverlay/>
-            <MobileBanner/>
+        <div className="flex flex-col md:flex-row h-[90vh] w-screen self-center justify-center md:mt-0 mt-16">
+          <LeftSide/>
+          <RightSide/>
+            {/* <MobileBanner/> */}
         </div>
     )
 } 
--- src/components/home/products/products.jsx
+++ src/components/home/products/products.jsx
@@ -7,6 +7,7 @@ import { Icon } from "@iconify/react/dist/iconify.js";
 export default function Products() {
   //get items
   const [products, setProducts] = useState([]);
+  const [activeTab, setActiveTab] = useState(0);
   useEffect(() => {
     GetProducts();
   }, []);
@@ -56,7 +57,7 @@ export default function Products() {
   return (
     <div className="mt-32 flex flex-col gap-10 w-full overflow-hidden">
       <div className="flex flex-col lg:flex-row justify-between">
-        <SubHeader style="text-start" title="Always choose the best" />
+        <SubHeader style="text-start" title="We always Give the best." />
         <div className="sm:max-w-[300px]">
           <p>
             lhiueuife ugteuge huyeourgeuy egroueoref ggeufeyuey bghjge egeoregr
@@ -66,7 +67,7 @@ export default function Products() {
       <div className="flex gap-3 overflow-x-auto">
         {tabs.map((tab, index) => (
           <button
-            className="rounded-3xl px-6 py-3 text-base uppercase font-semibold bg-transparent border-2 border-black"
+            className={`rounded-3xl px-6 py-3 text-base uppercase font-semibold border-2  ${activeTab === index ? "bg-black text-white border-black" : "text-black/85 bg-transparent border-black/85"}`}
             key={index}
           >
             {tab.name}
